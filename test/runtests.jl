3e15ff7c27732d288eee5183ee50864f7b44d14b
Marc Gillioz <marc.gillioz@gmail.com>
2024-06-21 16:04:40 +0200

    remove obsolete test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,5 @@ using Test
 
 @testset "AramisAPI.jl" begin
 
-    @testset "test_global_variables" begin
-        @test isa(AramisAPI.initial_grid, String)
-        @test length(AramisAPI.initial_grid) > 0
-    end
-
 
 end
