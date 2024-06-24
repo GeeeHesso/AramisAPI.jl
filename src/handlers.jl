e99e7acd43752d628c991da946ee20b1fb6271d7
Marc Gillioz <marc.gillioz@gmail.com>
2024-06-24 11:53:44 +0200

    implement power flow and attack logic

--- src/handlers.jl
+++ src/handlers.jl
@@ -11,20 +11,21 @@ end
 
 
 function real_network(params::DateTime) :: Dict{String, Any}
-    grid = parse_file(joinpath([MODULE_FOLDER, "networks", "other_grid.json"]))
-    # TODO: grid = deepcopy(INITIAL_GRID)
-    # TODO: update gens, loads, and lines (with or without Power Flow?)
-    return grid
+    network = parse_file(joinpath([MODULE_FOLDER, "networks", "other_grid.json"]))
+    # network = deepcopy(INITIAL_GRID)
+    # TODO: update gens and loads
+    powerflow!(network)
+    return network
 end
 
 
 function attacked_network(params::DateTimeAttack) :: Dict{String, Any}
-    grid = parse_file(joinpath([MODULE_FOLDER, "networks", "other_grid.json"]))
-    # TODO: grid = deepcopy(INITIAL_GRID)
-    # TODO: update gens, loads, and lines (with or without Power Flow?)
-    # TODO: create attack on each generator
-    # TODO: perform power flow
-    return grid
+    network = parse_file(joinpath([MODULE_FOLDER, "networks", "other_grid.json"]))
+    # network = deepcopy(INITIAL_GRID)
+    # TODO: update gens and loads
+    attack!(network, params.attacked_gens)
+    powerflow!(network)
+    return network
 end
 
 
@@ -45,4 +46,37 @@ function algorithms(params::DateTimeAttackAlgo) :: Dict{String, Any}
             "927" => false
         )
     )
+end
+
+
+function attack!(network::Dict{String, Any}, gen_id::String)
+    # identify the attacked generator
+    gen = network["gen"][gen_id]
+    p_max = gen["pmax"]
+    p_before = gen["pg"]
+    # change its active power generation
+    p_after = p_before < p_max / 2 ? p_max : 0.0
+    gen["pg"] = p_after
+    # distribute the change onto the slack loads so that the power balance is respected
+    p_diff = p_after - p_before / length(SLACK_LOADS)
+    for load_id in SLACK_LOADS
+        network["load"][load_id]["pd"] += p_diff
+    end
+    nothing
+end
+
+function attack!(network::Dict{String, Any}, gen_ids::Vector{String})
+    for gen_id in gen_ids
+        attack!(network, gen_id)
+    end
+    nothing
+end
+
+
+function powerflow!(network::Dict{String, Any})
+    pf = compute_dc_pf(network)
+    update_data!(network, pf["solution"])
+    flows = calc_branch_flow_ac(network)
+    update_data!(network, flows)
+    nothing
 end
